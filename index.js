ee1e1fc63447a7e5041b49245478943405bd46ee
ec <varikvi@yahoo.com>
2018-07-02 13:33:20 +0200

    correcting Social Authentication with Twitter: no forwarding

--- index.js
+++ index.js
@@ -54,13 +54,15 @@ auth_exer_app.use('/static', express.static('data'));
 
 // passport config
 //var passportApp = require('./server/routes/middleware').passportApp;
+// think about the configuration suggested in this tutorial: https://scotch.io/tutorials/easy-node-authentication-setup-and-local
+
 
 // a-
 // instantiating the mounting of the strategy into mongoDB through 'Account' schema
 var mw = require('./server/routes/middleware');
 
 auth_exer_app.use(mw.passportAppconfig.passportApp.initialize());
-auth_exer_app.use(mw.passportAppconfig.passportApp.session());
+auth_exer_app.use(mw.passportAppconfig.passportApp.session()); // persistent login sessions
 
 var localStrategy = require('passport-local').Strategy;
 mw.passportAppconfig.passportApp.use(new localStrategy({
@@ -70,18 +72,33 @@ mw.passportAppconfig.passportApp.use(new localStrategy({
     mw.passportAppconfig.Account.authenticate()
 ));
 
+
+var SchemaObject = require('node-schema-object');
+
 //https://developer.twitter.com/en/docs/twitter-for-websites/log-in-with-twitter/guides/implementing-sign-in-with-twitter
 var twitterStrategy = require('passport-twitter').Strategy;
 //console.log(config.twitterOAuth.consumer_key, config.ngrokcallback.callback);
 mw.passportAppconfig.passportApp.use(new twitterStrategy({
         consumerKey: config.twitterOAuth.consumer_key,
         consumerSecret: config.twitterOAuth.consumer_secret,
-        callbackURL: config.ngrokcallback.callback
+        callbackURL: config.twittercallback.callback
     },
-    function(token, tokenSecret, profile, done) {
-        console.log("ök!!");
-        done();
-    }));
+    function(token, tokenSecret, profile, done) { //validation function
+        console.log("this user is correctly validated!!");
+        done(null, { 'username': 'whatever' });
+        //var newUser = new SchemaObject({
+        //    name: String
+        //});
+        //var user = newUser({ name: "iamanuser" });
+        //done(null, user);
+        //user.get = function() {
+        //    return { username: "iamanuser" }
+        //};
+        //mw.passportAppconfig.Account.find({}, function(err, doc) {
+        //    done(null, doc);
+        //});
+    }
+));
 
 
 // Configure Passport authenticated session persistence (from https://github.com/passport/express-4.x-local-example/blob/master/server.js).
@@ -124,31 +141,33 @@ auth_exer_app.get('/auth/twitter', //from here we call Twitter and set it on in 
 // in order to see this working:
 // 1) set your Twitter App settings (currently using one I called "amsterdamfcctest01") for local development test
 //    see this: https://stackoverflow.com/a/31716180
-// 2) verify that the following requirements are correct in your config file:
+// 2) verify that the following requirements are correct in your config file AND your app settings:
 //  -- consumer key
 //  -- consumer secret
-//  -- updated (random) forwarding link from ngrok as callback link
-// 3) after that is verified, use the ngrok callback instead to get into twitter by using the params to get there, not your application
-// 4) if effectively redirected to Twitter, you will see the following:
-//    -- https://api.twitter.com/oauth/authorize?oauth_token=h9Xi3QAAAAAAudmNAAABZEXYcy8 //see there is an oauth_token for a requested authorization
-// 5) if authorization is given:
-//    -- 
+//  -- set web and callback using IP address for localhost instead of alias
+// 3) after that is verified, use the IP address instead of the alias to call the page from browser
 
 //here is where authorization is asked; if the requirements for the authentication are available, it runs the Twitter Strategy callback function
 auth_exer_app.get('/request-token',
     mw.passportAppconfig.passportApp.authenticate('twitter', {
         'session': true,
         successRedirect: '/',
-        failureRedirect: '/'
-    }));
+        failureRedirect: '/failure',
+    }),
+    function(res, req, next) {
+        console.log('OK redirect')
+    }
+);
 
 // The following errors are possible:
+// - I was using a proxy in ngrok: it is apparently not working and even considered malware
 // - URL considered malware: this is probably because you put the ngrok in the settings but you are not using the ngrok callback to get into twitter but trying to get there through the application instead; this error is POORLY documented!!!
 // - the URL is not recognised: probably you are using standard links in the settings but trying to get in contact with twitter through your app; this error is POORLY documented
 // - Callback URL not approved for this client application: there is a mismatch between the callbacks you have in config and the one you set in the App settings; your ngrok callback in config should match the one you provided as callback in twitter app settings
 // - twitter failed to authorize: if you are trying to link to twitter through ngrok link as recommended, it is probably the keys are incorrect: https://www.quora.com/Why-am-I-getting-the-Could-not-authenticate-you-message-from-Twitters-API
 // - failed to find request token in session: again, all the previous connecting procedure are ok but you don't have a session available; check this link: https://stackoverflow.com/questions/11075629/passport-twitter-failed-to-find-request-token-in-session  
 // ---- mounting passport.session() into the application will help
+// ---- see also more about securing passport to work at: https://github.com/expressjs/session/issues/281#issuecomment-191327863
 
 
 /////////////////////
